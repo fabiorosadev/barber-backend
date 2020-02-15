831ae69558d394b0707c9fd1a8d94f21c62b7c00
Fabio Rosa <fabiorosa.net@gmail.com>
2020-02-15 10:02:37 -0300

    Appointments

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,62 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, endOfHour } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { provider_id, date } = req.body;
+
+    const isProvider = await User.findOne({ where: { id: provider_id, provider: true } });
+    if (!isProvider)
+      return res.status(401).json({ error: 'You can only create appointments with providers' });
+
+    /**
+     * Check for pass dates
+     */
+    const hourStart = startOfHour(parseISO(date));
+    const hourEnd = endOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Pass dates are not permitted!' });
+    }
+
+    /**
+     * Check date availability
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceledAt: null,
+        date: {
+          [Op.between]: [hourStart, hourEnd],
+        },
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not available' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
